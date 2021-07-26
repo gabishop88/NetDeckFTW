ae527efea9c3da6dfdb1c800a604cbea7f38fa81
Graham Bishop <gabishop15@gmail.com>
2021-07-26 15:29:38 -0500

    Got basic CRUD elements working

--- db-interface/index.js
+++ db-interface/index.js
@@ -3,6 +3,9 @@ const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
 
+const { v4: uuidv4 } = require('uuid');;
+// import { v4 as uuidv4 } from 'uuid';
+
 var db = mysql.createConnection({
     host: '34.72.77.48',
     user: 'root',
@@ -14,13 +17,20 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+/** ========== Test DB Connection ========== */
+
+var test_err = '<h1>Please start SQL database.</h1>';
+var test_success = '<h1>Database connected.</h1>';
 app.get('/', (req, res) => {
     const query = "SELECT COUNT(*) AS count FROM `Users`";
     db.query(query, (err, result) => {
-        if (err) res.send('<h1>Please start SQL database.</h1>');
-        else res.send('<h1>Database connected.</h1>');
+        console.log(err);
+        if (err) res.send(test_err);
+        else res.send(test_success);
     })
 });
+
+/** ========== Handle Login Queries ========== */
 
 var checkuser_err = [];
 app.get('/db/checkuser', (req, res) => {
@@ -52,12 +62,14 @@ app.post('/db/adduser', (req, res) => {
     });
 });
 
-var cardsearch_err = [{CardName: 'Festive Elf', ManaCost: '4{G}{G}'}, {CardName: 'Santa\'s Helper', ManaCost: '4{G}{G}'}, {CardName: 'Gift Horse', ManaCost: '{1}{G}'}];
+/** ========== Handle Queries for Cards/Decks ========== */
+
+var cardsearch_err = [{DetailID: 123, CardName: 'Festive Elf', ManaCost: '4{G}{G}'}, {DetailID: 456, CardName: 'Santa\'s Helper', ManaCost: '4{G}{G}'}, {DetailID: 789, CardName: 'Gift Horse', ManaCost: '{1}{G}'}];
 app.post('/db/cardsearch', (req, res) => {
-    var q = 'SELECT CardName, ManaCost FROM CardDetails ';
+    var q = 'SELECT DetailID, CardName, ManaCost FROM CardDetails ';
     if ('name' in req.body) {
         if (req.body.name == '') {
-            q = "SELECT CardName, ManaCost, COUNT(DetailID) AS num_cards FROM CardDetails NATURAL JOIN DeckContains WHERE type NOT LIKE '%Basic%' GROUP BY DetailID ORDER BY num_cards DESC ";
+            q = "SELECT DetailID, CardName, ManaCost, COUNT(DetailID) AS num_cards FROM CardDetails NATURAL JOIN DeckContains WHERE type NOT LIKE '%Basic%' GROUP BY DetailID ORDER BY num_cards DESC ";
         } else {
             q = q.concat('WHERE CardName LIKE \'%' + req.body.name + '%\' ');
         }
@@ -65,15 +77,113 @@ app.post('/db/cardsearch', (req, res) => {
     q = q.concat('LIMIT 10');
     
     db.query(q, [], (err, result) => {
-        console.log(result);
         if (err) res.send(cardsearch_err);
         else res.send(result);
     })
 });
 
-app.get('/db/getdecks', (req, res) => {
-    const q = "";
+var getgroup_err = [{Name: 'E1', ID: "e1"}, {Name: 'E2', ID: 'e2'}];
+app.get('/db/getgroups', (req, res) => {
+    var type = req.query.type.substr(0, req.query.type.length - 1);
+    var owner = req.query.owner;
+    const q = `SELECT ${type}Name AS name, ${type}ID AS id, Description AS 'desc' FROM ${type}s WHERE Owner LIKE ?`;
+
+    db.query(q, [owner], (err, result) => {
+        if (err) res.send(getgroup_err.map(e => { return {name: e['Name'].replace(/E/gi, type), id: e["ID"], desc: ''} }));
+        else res.send(result);
+    })
 });
+
+app.get('/db/groupcards', (req, res) => {
+    var id = req.query.id;
+    var q = '';
+    if (req.query.type == 'Decks') {
+        q = "SELECT DetailID as id, Quantity AS qty, Location AS loc FROM DeckContains WHERE DeckID=?";
+    } else if (req.query.type == 'Collections') {
+        q = "SELECT CardID as id, Quantity AS qty, Foil AS foil FROM CollectionContains WHERE CollectionID=?";
+    }
+    db.query(q, [id], (err, result) => {
+        if (err) res.send([]);
+        else res.send(result);
+    });
+});
+
+app.post('/db/updategroup', (req, res) => {
+    var q = '';
+    console.log("Updating a group.");
+    if (req.query.type == "Decks") {
+        let translator = {
+            visibility: 'Visibility',
+            format: 'Format',
+            placing: 'Placing',
+            name: 'DeckName',
+            desc: 'Description'
+        }
+        q = "UPDATE IGNORE Decks SET ATTRIBUTES WHERE DeckID=?";
+        var str = '';
+        Object.keys(req.body).forEach(i => str = str.concat(translator[i], "='", req.body[i], "', "));
+        q = q.replace('ATTRIBUTES', str.slice(0, -2));
+        console.log(q, req.query.id);
+        db.query(q, [req.query.id], (err, result) => {
+            if (err) res.send("Update Failed");
+            else res.send("Updated");
+        });
+    } else if (req.query.type == "Collections") {
+        let translator = {
+            visibility: 'Visibility',
+            name: 'CollectionName',
+            desc: 'Description'
+        }
+        q = "UPDATE IGNORE Collections SET ATTRIBUTES WHERE CollectionID='?'";
+        var str = '';
+        Object.keys(req.body).forEach(i => str = str.concat(translator[i], "='", req.body[i], "', "));
+        q = q.replace('ATTRIBUTES', str.slice(0, -2));
+        db.query(q, [req.query.id], (err, result) => {
+            if (err) res.send("Update Failed");
+            else res.send("Updated");
+        });
+    }
+});
+
+app.post('/db/addgroup', (req, res) => {
+    let groupid = uuidv4();
+    let type = req.body.type.slice(0, -1);
+    const q = `INSERT INTO ${type}s (${type}ID, Owner) VALUES (?, ?)`;
+    console.log('adding a new deck.');
+    db.query(q, [groupid, req.body.owner], (err, result) => {
+        console.log(err);
+        if (err) res.send("Creation Failed");
+        else {
+            db.query(`SELECT ${type}ID AS id, ${type}Name AS name, Description AS 'desc' FROM ${type}s WHERE ${type}ID=?`, [groupid], (err, result) => {
+                console.log(err);
+                console.log(result);
+                res.send(result);
+            });
+        }
+    })
+});
+
+app.delete('/db/deletegroup/:groupid', (req, res) => {
+    var id = req.params.groupid;
+    var type = req.query.type.slice(0, -1);
+    const q = `DELETE FROM ${type}s WHERE ${type}ID=?`;
+    db.query(q, [id], (err, result) => {
+        if (err) res.send("Could not delete");
+        else res.send("Done");
+    });
+});
+
+app.get('/db/getrecommendations/:detailID', (req, res) => {
+    var id = req.params.detailID;
+    var format = req.query.format;
+    const q = "CALL recommend_cards(?, ?)";
+    db.query(q, [id, format], (err, result) => {
+        if (err) res.send([]);
+        else res.send(result);
+    });
+});
+
+/** ========== Start Server ========== */
 
 app.listen(3002, () => {
     console.log("Server Started at http://localhost:3002");
--- netdeckftw/src/pages/Home.js
+++ netdeckftw/src/pages/Home.js
@@ -1,26 +1,79 @@
 import { Redirect } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../stylesheets/Home.scss';
 import Axios from "axios";
 const url = 'http://localhost:3002';
 
 function Home(state) {
     const [cardResults, setCardResults] = useState([]);
-    const [ownedDecks, setOwnedDecks] = useState([]);
-    const [selected, setSelected] = useState('');
+    const [ownedGroups, setOwnedGroups] = useState([]);
+    const [selectedGroup, setSelectedGroup] = useState({name: null, id: '', desc: '', cards: []});
+    const [selectedGroupType, setSelectedGroupType] = useState('Decks');
+    const [recommendations, setRecommendations] = useState([]);
 
     function updateCardList(e) {
         var cardName = '';
-        if (e) {
-            cardName = e.target.value;
-        }
-        
-
+        if (e) cardName = e.target.value;
         Axios.post(url + '/db/cardsearch', {name: cardName.replaceAll(/'/ig, "''")}).then(result => {
             setCardResults(result.data);
+            if (result.data.length === 1) { // OR name matches a result
+                let format = 'none';
+                if (selectedGroup.name)  {
+                    // Get request for format OR store format in selectedGroup
+                }
+                Axios.get(url + `/db/getrecommendations/${result.data[0].DetailID}?format=${format}`).then(result => {
+                    console.log(result.data[0]);
+                    setRecommendations(result.data[0]);
+                });
+            }
         });
     }
-    if (cardResults.length === 0) updateCardList(null);
+    if (cardResults.length === 0) updateCardList(null); 
+
+    useEffect(() => {
+        async function getOwned() {
+            Axios.get(url + '/db/getgroups?type=' + selectedGroupType + '&owner=' + state.userName).then(result => {
+                result.data.push({name: '+', id: 'creator'});
+                setOwnedGroups(result.data);
+            });
+        }
+        getOwned();
+    }, [selectedGroupType, state.userName, selectedGroup]);
+
+    function selectGroup(group) {
+        if (group.id === 'creator') {
+            Axios.post(url + '/db/addgroup', {type: selectedGroupType, owner: state.userName}).then(result => {
+                if (result.data === 'Creation Failed') {
+                    return; // Error messages?
+                }
+                Axios.get(url + '/db/groupcards?type=' + selectedGroupType + '&id=' + result.data[0].id).then(res2 => {
+                    setSelectedGroup({name: result.data[0].name, id: result.data[0].id, desc: result.data[0].desc, cards: res2.data});
+                });
+            });
+        } else {
+            Axios.get(url + '/db/groupcards?type=' + selectedGroupType + '&id=' + group.id).then(result => {
+                setSelectedGroup({name: group.name, id: group.id, desc: group.desc, cards: result.data});
+            });
+        }
+    }
+
+    function updateGroup(change) {
+        Axios.post(url + '/db/updategroup?type=' + selectedGroupType + '&id=' + selectedGroup.id, change).then(result => {
+            setSelectedGroup({
+                name: (change.hasOwnProperty('name') ? change.name : selectedGroup.name),
+                id: selectedGroup.id,
+                desc: (change.hasOwnProperty('desc') ? change.desc : selectedGroup.desc),
+                cards: selectedGroup.cards
+            });
+        });
+    }
+
+    function deleteGroup(id) {
+        // Eventually maybe have a confirmation?
+        Axios.delete(url + `/db/deletegroup/${id}?type=${selectedGroupType}`).then(result => {
+            if (result.data === "Done") setSelectedGroup({name: null, id: '', desc: '', cards: []});
+        });
+    }
 
     return state.userName === '' ? 
     (
@@ -33,7 +86,7 @@ function Home(state) {
                         {
                             cardResults.map((elem) => {
                                 return (
-                                    <li>{elem.CardName}</li>
+                                    <li key={elem.DetailID}>{elem.CardName}</li>
                                 );
                             })
                         }
@@ -45,23 +98,36 @@ function Home(state) {
                 </div>
                 <div className='workspace flex-col'>
                     <div className='display-cards'>
-                        Something
+                        <input type='text' value={selectedGroup.name != null ? selectedGroup.name : 'None Selected'} readOnly={selectedGroup.name == null} onChange={e => {updateGroup({name: e.target.value})}}></input>
+                        <button onClick={e => deleteGroup(selectedGroup.id)}>DELETE</button>
                     </div>
-                    <div className='owned-groups'>
-                        <p>Something else</p>
-                        <ul className='owned-decks'>
+                    <div className='owned'>
+                        <div className='type-selection'>
+                            <p>{state.userName}'s </p>
+                            <p className='selected-group' onClick={e => setSelectedGroupType(selectedGroupType === 'Decks' ? 'Collections' : 'Decks')}>{selectedGroupType}</p>
+                        </div>
+                        <div className='owned-groups'>
                             {
-                                ownedDecks.map(deck => {
+                                ownedGroups.map(group => {
                                     return (
-                                        <li id={'Deck#' + deck.deckID} onClick={e => setSelected(e.target.id)}>{deck.DeckName}</li>
+                                        <div key={group.id}>
+                                            <p className='group-entry' onClick={e => selectGroup(group)}>{group.name}</p>
+                                        </div>
                                     );
                                 })
                             }
-                        </ul>
+                        </div>
                     </div>
                 </div>
                 <div className='extras-panel flex-col'>
-                    <p>Usage TBD</p>
+                    <p>Recommendations</p>
+                    {
+                        recommendations.map(card => {
+                            return (
+                                <li key={card.DetailID}>{card.CardName}</li>
+                            );
+                        })
+                    }
                 </div>
             </div>
         </div>
